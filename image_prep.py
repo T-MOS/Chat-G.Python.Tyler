a0a7401857b31fa4627df3eaad71e06252913988
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-01-10 12:53:46 -0600

    Update image_prep.py
    migrating scaler

--- image_prep.py
+++ image_prep.py
@@ -1,1 +1,15 @@
+from PIL import Image, ImageTk
+from Xlib import display
 
+image = Image.open("ascii/enceladusstripes_cassini_forBG.jpg")
+scale_factor = float(
+    (display.Display().screen().width_in_pixels / image.size[0]))
+
+def scale_me(image, scale_factor):
+  width, height = image.size
+  new_width = int(width * scale_factor)
+  new_image = image.resize((new_width, int(height * scale_factor)))
+  new_image.show()
+  return new_image
+
+scale_me(image, scale_factor)
