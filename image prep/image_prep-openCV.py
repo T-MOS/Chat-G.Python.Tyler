2a2a69b3328fd77f0015d79237c48f3d61f1e7c4
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-01-10 18:45:28 -0600

    Update image_prep-openCV.py
    added Scharr 3x3 pass for comparison

--- image prep/image_prep-openCV.py
+++ image prep/image_prep-openCV.py
@@ -13,7 +13,7 @@ img = cv.imread('2401.jpg', cv.IMREAD_GRAYSCALE)
 (h, w) = img.shape
 assert img is not None, "file could not be read, check with os.path.exitsts()"
 
-#scale w/ aspect
+
 scale_factor = (550 / w)
 scaled = (550, int(h * scale_factor))
 resized = cv.resize(img, scaled)
@@ -26,8 +26,11 @@ cv.imshow("display img", resized)
 
 laplacian = cv.Laplacian(resized, cv.CV_64F)
 
-#might try next
-# scharr =
+sobel_scharrx = cv.Sobel(resized, cv.CV_64F, 1, 0, ksize=-1)
+sobel_scharry = cv.Sobel(resized, cv.CV_64F, 0, 1, ksize=-1)
+abs_sobel_scharrx = np.abs(sobel_scharrx)
+abs_sobel_scharry = np.abs(sobel_scharry)
+
 
 sobelx = cv.Sobel(resized,cv.CV_64F, 1, 0, ksize=5)
 abs_sobelx = np.abs(sobelx)
@@ -35,16 +38,22 @@ abs_sobelx = np.abs(sobelx)
 sobely = cv.Sobel(resized,cv.CV_64F, 0, 1, ksize=5)
 abs_sobely = np.abs(sobely)
 
-
-# dtype output of uint8 (CV_8U) of |64F| is garbage
-# sobel8ux = np.uint8(abs_sobelx)
-# sobel8uy = np.uint8(abs_sobely)
+sobel8ux = np.uint8(abs_sobelx)
+sobel8uy = np.uint8(abs_sobely)
 
 plt.subplot(1,3,1),plt.imshow(resized,cmap = 'gray')
 plt.title('Original, scaled.'), plt.xticks([]), plt.yticks([])
-plt.subplot(1,3,2),plt.imshow(abs_sobelx,cmap = 'gray')
+# plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+# plt.title('Laplacian 2nd Derivative'), plt.xticks([]), plt.yticks([])
+
+# plt.subplot(1,3,2),plt.imshow(abs_sobelx,cmap = 'gray')
+# plt.title('Sobel X-axis'), plt.xticks([]), plt.yticks([])
+# plt.subplot(1,3,3),plt.imshow(abs_sobely,cmap = 'gray')
+# plt.title('Sobel Y-axis'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(1,3,2),plt.imshow(abs_sobel_scharrx,cmap = 'gray')
 plt.title('Sobel X-axis'), plt.xticks([]), plt.yticks([])
-plt.subplot(1,3,3),plt.imshow(abs_sobely,cmap = 'gray')
+plt.subplot(1,3,3),plt.imshow(abs_sobel_scharry,cmap = 'gray')
 plt.title('Sobel Y-axis'), plt.xticks([]), plt.yticks([])
  
 
