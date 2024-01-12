ee09241886a64e3ca8efbb33f27f66c679d03876
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-01-11 20:42:47 -0600

    Update image_prep-openCV.py
    lots and also little

--- image prep/image_prep-openCV.py
+++ image prep/image_prep-openCV.py
@@ -22,16 +22,15 @@ if response.status_code == 200:
         # Find image tag and get source URL
     img_tag = soup.find('img')
     img_url = 'https://apod.nasa.gov/apod/' + img_tag['src']
-    # Request the image and open it with PIL
+    # Request the image
     image_response = requests.get(img_url)
     if image_response.status_code == 200:
-        # image_buffer = BytesIO()
         image_array = np.frombuffer(image_response.content, np.uint8)
         image = cv.imdecode(image_array, cv.IMREAD_COLOR)
         img = cv.imdecode(image_array, cv.IMREAD_GRAYSCALE)
         assert image is not None, "file  not found, 'os.path.exitsts()'?"
         (h, w) = img.shape
-        print("height is {h}, width is {w}")
+        print(f"height is {h!r}, width is {w!r}")
 
 scale_factor = (550 / w)
 scaled = (550, int(h * scale_factor))
@@ -43,41 +42,26 @@ for_kernels = cv.resize(img,scaled)
 if img is None:
     sys.exit("Could not read the image")
 
-cv.imshow("display img", resized)
-
-#laplacian = cv.Laplacian(resized, cv.CV_64F)
-# sobel_scharrx = cv.Sobel(resized, cv.CV_64F, 1, 0, ksize=-1)
-# sobel_scharry = cv.Sobel(resized, cv.CV_64F, 0, 1, ksize=-1)
-# abs_sobel_scharrx = np.abs(sobel_scharrx)
-# abs_sobel_scharry = np.abs(sobel_scharry)
+def show_colored_array(array):
+    pos_color = np.array(0.36, 0.82, 0.8)
+    neg_color = np.array(0.99,0.18,0.13)
+    def to_rgb(x):
+        return np.maximum(x, 0) * pos_color + np.maximum(-x, 0) * neg_color
+    return np.arrray([to_rgb(val) for val in array]) / np.max(np.abs(array))
 
 
 sobelx = cv.Sobel(for_kernels,cv.CV_64F, 1, 0, ksize=5)
 abs_sobelx = np.abs(sobelx)
-
 sobely = cv.Sobel(for_kernels,cv.CV_64F, 0, 1, ksize=5)
 abs_sobely = np.abs(sobely)
 
+# print(f"sqrt sqrds: {np.sqrt((sobelx**2 + sobely**2))!r}")
 
-plt.subplot(1,3,1),plt.imshow(resized,cmap = 'gray')
-plt.title('Original, scaled.'), plt.xticks([]), plt.yticks([])
-# plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-# plt.title('Laplacian 2nd Derivative'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(1,3,2),plt.imshow(abs_sobelx,cmap = 'gray')
-plt.title('Sobel X-axis'), plt.xticks([]), plt.yticks([])
-plt.subplot(1,3,3),plt.imshow(abs_sobely,cmap = 'gray')
-plt.title('Sobel Y-axis'), plt.xticks([]), plt.yticks([])
-
-# plt.subplot(1,3,2),plt.imshow(abs_sobel_scharrx,cmap = 'gray')
+# plt.subplot(1,3,1),plt.imshow(resized,cmap = 'gray')
+# plt.title('Original, scaled.'), plt.xticks([]), plt.yticks([])
+# plt.subplot(1,3,2),plt.imshow(abs_sobelx,cmap = 'gray')
 # plt.title('Sobel X-axis'), plt.xticks([]), plt.yticks([])
-# plt.subplot(1,3,3),plt.imshow(abs_sobel_scharry,cmap = 'gray')
+# plt.subplot(1,3,3),plt.imshow(abs_sobely,cmap = 'gray')
 # plt.title('Sobel Y-axis'), plt.xticks([]), plt.yticks([])
-
-
-plt.show()
-
-# k = cv.waitKey(0)
-
-# if k == ord('s'):
-#     cv.imwrite('deepField.jpg', img)
+# plt.show()
+# cv.imshow("display image", resized)
